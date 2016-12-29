32f715a3fdc30baad025918055fd1dc29520a39b
anderzzz <ohrn.anders@gmail.com>
2016-12-29 12:19:57 -0800

    Migrate model

--- protein_img_api/settings.py
+++ protein_img_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'protein_imgs.apps.ProteinImgsConfig'
 ]
 
 MIDDLEWARE = [
--- protein_imgs/models.py
+++ protein_imgs/models.py
@@ -1,3 +1,7 @@
+'''Model definitions, which corresponds to how the data is structured and
+defined, as it is exposed to the API.
+
+'''
 from django.db import models
 from django.core import validators
 
@@ -5,9 +9,19 @@ class ProteinDataViz(models.Model):
     '''Django model for the data visualization
 
     '''
+    WHO_ENTERED = (
+        ('H1', 'Human One'),
+        ('H2', 'Human Second'),
+        ('S1', 'Software One'))
+
     created = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=200, blank=True, default='')
+    created_by = models.CharField(max_length=200, blank=True, default='',
+                                  choices=WHO_ENTERED)
     created_by_version = models.CharField(max_length=200, blank=False)
     created_by_version.validators = [validators.int_list_validator(sep='.')]
     viz_category = models.CharField(max_length=200, blank=True, default='')
     vis_file = models.FileField(upload_to='uploads/%Y/%m/%d')
+
+    class Meta:
+        '''Meta class to define attribute to order'''
+        ordering = ('created',)
