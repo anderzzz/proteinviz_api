a906363678ad28d5b4d41ad5f009d17ee24af895
anderzzz <ohrn.anders@gmail.com>
2017-01-11 20:05:47 -0800

    Begin renaming process for REST API

---
+++ presenter_webapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PresenterAppConfig(AppConfig):
+    name = 'presenter_webapp'
--- protein_imgs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProteinImgsConfig(AppConfig):
-    name = 'protein_imgs'
